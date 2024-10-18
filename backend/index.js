fe30e0f6fd606fda22bc63e550eef800a34fefac
Alex <01.alexsandr.46@gmail.com>
2024-10-18 15:28:33 +0500

    FirstCommit

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,14 @@
 require('dotenv').config();
-const { Telegraf, Markup } = require('telegraf');
+const {Telegraf, Markup} = require('telegraf');
 
 const bot = new Telegraf(process.env.TOKEN);
 
 bot.start((ctx) => {
     ctx.replyWithHTML(`
-Hello, <b>${ctx.from.first_name}</b>! 👋
-
-`, 
-    Markup.inlineKeyboard([
-        Markup.button.url('Open web app', process.env.WEB_URL)
-    ]));
+        Hello, <b>${ctx.from.first_name}</b>! 👋 `,
+        Markup.inlineKeyboard([
+            Markup.button.url('Open web app', process.env.WEB_URL)
+        ]));
 });
 
 bot.launch({
