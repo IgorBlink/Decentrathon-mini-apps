a858dac17d8c788cb7a241a96f4ecc71131cf14f
honley1 <mfhonley@gmail.com>
2024-10-18 12:52:32 +0500

    aaaa

--- backend/index.js
+++ backend/index.js
@@ -7,16 +7,9 @@ bot.start((ctx) => {
     ctx.replyWithHTML(`
 Hello, <b>${ctx.from.first_name}</b>! 👋
 
-Welcome to <b>TravelService</b> - your personal assistant in the world of travel! 🌍✈️
-
-Here you can:
-    - Find the best deals on tours and hotels 🏨
-    - Find out information about attractions 🏰
-    - Get recommendations for places to visit 🗺️
-
-Ready to start your journey? 🚀`, 
+`, 
     Markup.inlineKeyboard([
-        Markup.button.url('Open web app', 'https://t.me/travelservicebot/travelservice')
+        Markup.button.url('Open web app', process.env.WEB_URL)
     ]));
 });
 
